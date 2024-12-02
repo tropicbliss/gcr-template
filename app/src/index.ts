6fb99bd30331959a0d20056c8ca5e8da95e20c68
tropicbliss <tropicbliss@protonmail.com>
2024-12-02 18:02:19 +0800

    E

--- app/src/index.ts
+++ app/src/index.ts
@@ -4,7 +4,7 @@ import { Hono } from "hono";
 const app = new Hono();
 const port = parseInt(process.env.PORT || "8000");
 
-app.get("/", (c) => c.text("Hello Vibefam!"));
+app.get("/", (c) => c.text("Hello everyone!"));
 
 serve({
     fetch: app.fetch,
