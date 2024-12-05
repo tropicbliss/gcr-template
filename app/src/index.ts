d8be7f3dc2cf7453e037477d868ad81c6b7288f1
tropicbliss <tropicbliss@protonmail.com>
2024-12-05 15:25:11 +0800

    Revert

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,6 +1,5 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import { SecretManagerServiceClient } from "@google-cloud/secret-manager"
 
 const app = new Hono();
 const port = parseInt(process.env.PORT || "8000");
